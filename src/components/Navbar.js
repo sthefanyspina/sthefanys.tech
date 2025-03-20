d2e3e14d15db1666f5a7a9b30a0c01d705170c4d
Sthefany Spina <125087195+sthefanyspina@users.noreply.github.com>
2025-03-20 06:45:05 -0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -91,6 +91,17 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
+            <Nav.Item>
+              <Nav.Link
+                href="https://dev.to/sthefanyspina"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <ImBlog style={{ marginBottom: "2px" }} /> Blogs
+              </Nav.Link>
+            </Nav.Item>
+
+
             <Nav.Item className="fork-btn">
               <Button
                 href="https://github.com/sthefanyspina/portfolioSthefanySpina"
