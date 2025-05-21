c4306978ca19efb1ef5814e39efa2b9161a79979
Sthefany Spina <125087195+sthefanyspina@users.noreply.github.com>
2025-05-21 07:20:07 -0300

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -34,30 +34,30 @@ function About() {
           </Col>
         </Row>
 
-        function Certifications() {
-          return (
-            <Container fluid className="about-section">
-              <Particle />
-              <Container>
-                <Row style={{ justifyContent: "center", padding: "10px" }}>
-                  <Col
-                    md={7}
-                    style={{
-                    justifyContent: "center",
-                    paddingTop: "30px",
-                    paddingBottom: "50px",
-                    }}
-                  >
-                    <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-                    <strong className="purple">Certifications 
-                    and <strong className="purple">Licenses</strong>
-                    </h1>
+      function Certifications() {
+        return (
+          <Container fluid className="about-section">
+            <Particle />
+            <Container>
+              <Row style={{ justifyContent: "center", padding: "10px" }}>
+                <Col
+                  md={7}
+                  style={{
+                  justifyContent: "center",
+                  paddingTop: "30px",
+                  paddingBottom: "50px",
+                  }}
+                >
+                  <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+                  <strong className="purple">Certifications 
+                  and <strong className="purple">Licenses</strong>
+                  </h1>
               
 
-            </Container>
           </Container>
-        );
-      }
+        </Container>
+      );
+    }
 
       function CertificationsCard() {
         return (
