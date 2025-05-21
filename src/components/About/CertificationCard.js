e3a5a88f40c081114486bb34fb4b08d543d5fbfc
Sthefany Spina <125087195+sthefanyspina@users.noreply.github.com>
2025-05-21 07:48:26 -0300

    Update CertificationCard.js

--- src/components/About/CertificationCard.js
+++ src/components/About/CertificationCard.js
@@ -6,27 +6,32 @@ function CertificationsCard() {
     <Card className="quote-card-view">
       <Card.Body>
         <blockquote className="blockquote mb-0">
-          <ul>
-            <li className="certications">
-              Databricks - <span className="purple">Databricks Fundamentals</span>
-            </li>
-            <li className="certications">
-              Google - <span className="purple">Data Analytics</span>
-            </li>
-            <li className="certications">
-              IBM - <span className="purple">Data Science Professional Certificate</span>
-            </li>
-            <li className="certications">
-              IBM - <span className="purple">Data Analyst Professional Certificate</span>
-            </li>
-            <li className="certications">
-              IBM - <span className="purple">Data Engineering Professional Certificate</span>
-            </li>
-          </ul>
-
+            <p style={{ textAlign: "justify" }}>
+                <ul>
+                    <li className="certications">
+                        Databricks - <span className="purple">Databricks Fundamentals</span>
+                    </li>
+                    
+                    <li className="certications">
+                        Google - <span className="purple">Data Analytics</span>
+                    </li>
+                    
+                    <li className="certications">
+                        IBM - <span className="purple">Data Science Professional Certificate</span>
+                    </li>
+            
+                    <li className="certications">
+                        IBM - <span className="purple">Data Analyst Professional Certificate</span>
+                    </li>
+                    
+                    <li className="certications">
+                        IBM - <span className="purple">Data Engineering Professional Certificate</span>
+                    </li>
+            </ul>
+            </p>
         </blockquote>
       </Card.Body>
     </Card>
   );
 }
-export default CertificationCard;
+export default CertificationsCard;
