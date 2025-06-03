5acf646226ad3ecb718c41db084ce523b5b3ba7b
Sthefany Spina <125087195+sthefanyspina@users.noreply.github.com>
2025-06-03 16:21:27 -0300

    Create BigData.js

---
+++ src/components/About/BigData.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiApachespark,
+  SiApachehadoop,
+  SiApachekafka,
+} from "react-icons/si";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiApachespark />
+      </Col>
+  
+      <Col xs={4} md={2} className="tech-icons">
+        <SiApachehadoop />
+      </Col>
+  
+      <Col xs={4} md={2} className="tech-icons">
+        <SiApachekafka />
+      </Col>
+
+    </Row>
+  );
+}
+
+export default BigData;
