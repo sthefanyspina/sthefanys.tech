2eb396ef2ff483ee4fa85190ad15798bca9af264
Sthefany Spina <125087195+sthefanyspina@users.noreply.github.com>
2025-06-03 16:08:33 -0300

    Create Databases.js

---
+++ src/components/About/Databases.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  DiMongodb,
+  DiMysql,
+  DiPostgresql,
+  DiRedis,
+} from "react-icons/di";
+import {
+  SiApachecassandra,
+} from "react-icons/si";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMysql />
+      </Col>
+  
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPostgresql />
+      </Col>
+  
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+      </Col>
+  
+      <Col xs={4} md={2} className="tech-icons">
+        <DiRedis />
+      </Col>
+  
+      <Col xs={4} md={2} className="tech-icons">
+        <SiApachecassandra />
+      </Col>
+
+    </Row>
+  );
+}
+
+export default Databases;
