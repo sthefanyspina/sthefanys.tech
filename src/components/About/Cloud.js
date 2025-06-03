a45e8c398c31a6ad4927460b88fa9c919f72105b
Sthefany Spina <125087195+sthefanyspina@users.noreply.github.com>
2025-06-03 16:24:19 -0300

    Create Cloud.js

---
+++ src/components/About/Cloud.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { FaAws,
+} from "react-icons/fa";
+import { VscAzure,
+} from "react-icons/vsc";
+import { BiLogoGoogleCloud,
+} from "react-icons/bi";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaAws />
+      </Col>
+  
+      <Col xs={4} md={2} className="tech-icons">
+        <VscAzure />
+      </Col>
+  
+      <Col xs={4} md={2} className="tech-icons">
+        <BiLogoGoogleCloud />
+      </Col>
+
+    </Row>
+  );
+}
+
+export default Cloud;
