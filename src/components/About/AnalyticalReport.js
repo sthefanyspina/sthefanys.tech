12db2547a21171dac1b246818438c54852eb5011
Sthefany Spina <125087195+sthefanyspina@users.noreply.github.com>
2025-06-03 16:18:52 -0300

    Create AnalyticalReport.js

---
+++ src/components/About/AnalyticalReport.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { 
+  FaRegFileExcel,
+} from "react-icons/fa";
+import {
+  SiTableau,
+  SiLooker,
+} from "react-icons/si";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaRegFileExcel />
+      </Col>
+  
+      <Col xs={4} md={2} className="tech-icons">
+        <SiTableau />
+      </Col>
+  
+      <Col xs={4} md={2} className="tech-icons">
+        <SiLooker />
+      </Col>
+
+    </Row>
+  );
+}
+
+export default AnalyticalReport;
