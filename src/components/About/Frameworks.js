479b6cc894da3cd4f6d61bb9cd1385b843a86859
Sthefany Spina <125087195+sthefanyspina@users.noreply.github.com>
2025-06-03 16:04:40 -0300

    Create Frameworks.js

---
+++ src/components/About/Frameworks.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  DiNodejs,
+} from "react-icons/di";
+import {
+  SiPandas,
+  SiNumpy,
+  SiFlask,
+  SiDjango,
+} from "react-icons/si";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPandas />
+      </Col>
+  
+      <Col xs={4} md={2} className="tech-icons">
+        <SiNumpy />
+      </Col>
+  
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFlask />
+      </Col>
+     
+      <Col xs={4} md={2} className="tech-icons">
+        <SiDjango />
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      
+
+    </Row>
+  );
+}
+
+export default Frameworks;
