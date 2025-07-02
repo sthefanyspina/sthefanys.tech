a996160bb10a18ba71c5de1b1e3dec513fd9d472
Sthefany Spina <125087195+sthefanyspina@users.noreply.github.com>
2025-07-02 12:49:10 -0300

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,6 +4,13 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import ApacheFlinkSalesAnalysis from "../../Assets/Projects/ApacheFlinkSalesAnalysis.png";
 import EMRDataProcessing from "../../Assets/Projects/EMRDataProcessing.png";
+import YouTubeAnalytics from "../../Assets/Projects/YouTubeAnalytics.png";
+import FootballDataEngineering from "../../Assets/Projects/FootballDataEngineering.png";
+import JapanVisaAnalysis from "../../Assets/Projects/JapanVisaAnalysis.png";
+import SalesModernDataEngineeringProject from "../../Assets/Projects/SalesModernDataEngineeringProject.png";
+import CiCd from "../../Assets/Projects/CiCd.png";
+import SimulatedFinancialTransactions from "../../Assets/Projects/SimulatedFinancialTransactions.png";
+import KubernetesDataEngineering from "../../Assets/Projects/KubernetesDataEngineering.png";
 import HRAnalyticsDashboard from "../../Assets/Projects/HRAnalyticsDashboard.png";
 import EcommerceSalesAnalysis from "../../Assets/Projects/EcommerceSalesAnalysis.png";
 import CarSales from "../../Assets/Projects/CarSales.png";
@@ -35,7 +42,7 @@ function Projects() {
               imgPath={EMRDataProcessing}
               isBlog={false}
               title="Apache Spark - EMR Data Processing"
-              description="TThis project demonstrates the use of Amazon Elastic Map Reduce (EMR) for processing large datasets using Apache Spark. It includes a Spark script for ETL (Extract, Transform, Load) operations, AWS command line instructions for setting up and managing the EMR cluster, and a dataset for testing and demonstration purposes."
+              description="This project demonstrates the use of Amazon Elastic Map Reduce (EMR) for processing large datasets using Apache Spark. It includes a Spark script for ETL (Extract, Transform, Load) operations, AWS command line instructions for setting up and managing the EMR cluster, and a dataset for testing and demonstration purposes."
               ghLink="https://github.com/sthefanyspina/DataEngineeringProjects/tree/main/Apache%20Spark/EMRDataProcessing"
             />
           </Col>
@@ -47,6 +54,76 @@ function Projects() {
               title="Apache Flink - Sales Analysis"
               description="This repository contains an end-to-end data engineering project using Apache Flink, focused on performing sales analytics. The project demonstrates how to ingest, process, and analyze sales data, showcasing the capabilities of Apache Flink for big data processing."
               ghLink="https://github.com/sthefanyspina/DataEngineeringProjects/tree/main/Apache%20Flink/SalesAnalytics"
+            />
+          </Col>
+
+            <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={YouTubeAnalytics}
+              isBlog={false}
+              title="YouTube Analytics"
+              description="This Python-based project aims to fetch real-time YouTube metrics like likes, views, comments, and favorites, and then streams this data via Kafka. Also, KSqlDB was used for stream processing and the processed data is then sent to a Telegram bot for real-time notifications."
+              ghLink="https://github.com/sthefanyspina/DataEngineeringProjects/tree/main/Google%20Cloud/Youtube_Analytics"
+            />
+          </Col>
+
+            <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={JapanVisaAnalysis}
+              isBlog={false}
+              title="Japan Visa Analysis"
+              description="This project provides an end-to-end data processing and visualization of visa numbers in Japan using PySpark and Plotly. The spark clusters are set up within a Docker container on Azure."
+              ghLink="https://github.com/sthefanyspina/DataEngineeringProjects/tree/main/Azure/Japan_visa"
+            />
+          </Col>
+
+            <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={FootballDataEngineering}
+              isBlog={false}
+              title="Football - Data Engineering Project"
+              description="This Python-based project crawls data from Wikipedia using Apache Airflow, cleans it and pushes it Azure Data Lake for processing."
+              ghLink="https://github.com/sthefanyspina/DataEngineeringProjects/tree/main/Azure/Football_wikipedia"
+            />
+          </Col>
+
+            <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={SalesModernDataEngineeringProject}
+              isBlog={false}
+              title="Modern Data Engineering Project"
+              description="In this project, we setup and end to end data engineering using Apache Spark, Azure Databricks, Data Build Tool (DBT) using Azure as our cloud provider. This project illustrate the process of data ingestion to the lakehouse, data integration with ADF and data transformation with Databricks, and DBT."
+              ghLink="https://github.com/sthefanyspina/DataEngineeringProjects/tree/main/Azure/sales_moder_data_engineering_project"
+            />
+          </Col>
+
+            <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={SimulatedFinancialTransactions}
+              isBlog={false}
+              title="Simulated Financial Transactions"
+              description="This Python script is designed to generate simulated financial transactions and insert them into a PostgreSQL database. It's particularly useful for setting up a test environment for Change Data Capture (CDC) with Debezium. The script uses the faker library to create realistic, yet fictitious, transaction data and inserts it into a PostgreSQL table."
+              ghLink="https://github.com/sthefanyspina/DataEngineeringProjects/tree/main/Kafka/Simulated_Financial_Transactions"
+            />
+          </Col>
+
+            <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={CiCd}
+              isBlog={false}
+              title="CI / CD Project"
+              description="This repository is an Infrastructure as Code (IaC) project that leverages terraform for orchestrating the provisioning of the Azure services related to Data Engineering."
+              ghLink="https://github.com/sthefanyspina/DataEngineeringProjects/tree/main/Azure/ci_cd"
+            />
+          </Col>
+
+            <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={KubernetesDataEngineering}
+              isBlog={false}
+              title="Kubernetes - Data Engineering Project"
+              description="This repository contains the necessary configuration files and DAGs (Directed Acyclic Graphs) for setting up a robust data engineering environment using Kubernetes and Apache Airflow. It includes the setup for the Kubernetes Dashboard, which provides a user-friendly web interface for managing Kubernetes clusters, and Apache Airflow, a platform to programmatically author, schedule, and monitor workflows."
+              ghLink="https://github.com/sthefanyspina/DataEngineeringProjects/tree/main/Kubernetes/Kubernetes_Data_Engineering"
             />
           </Col>
                 
