69d033c588817228f4895ea70668fc06a967b8de
Sthefany Spina <125087195+sthefanyspina@users.noreply.github.com>
2025-08-07 07:31:35 -0300

    Update ETLTools.js

--- src/components/About/ETLTools.js
+++ src/components/About/ETLTools.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
   SiApacheairflow,
-  SiDbt,
 } from "react-icons/si";
 
 function ETLTools() {
@@ -10,10 +9,6 @@ function ETLTools() {
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <SiApacheairflow />
-      </Col>
-  
-      <Col xs={4} md={2} className="tech-icons">
-        <SiDbt />
       </Col>
 
     </Row>
