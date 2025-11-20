a627c545bc675c5e575600a39cec69f014a486f9
Sthefany Spina <125087195+sthefanyspina@users.noreply.github.com>
2025-11-20 14:37:20 -0300

    Refactor Projects component to add new project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -11,6 +11,7 @@ import LoanAnalysisDasboard from "../../Assets/Projects/analise_emprestimos.png"
 import RoadAccidentDashboard from "../../Assets/Projects/RoadAccidentDashboard.png";
 import CoffeSalesDashboard from "../../Assets/Projects/CoffeSalesDashboard.png";
 import WalmartSalesAnalysis from "../../Assets/Projects/WalmartSalesAnalysis.png";
+import DashboardPipeline from "../../Assets/Projects/dashboard_pipeline.png";
 
 function Projects() {
   return (
@@ -25,7 +26,28 @@ function Projects() {
         </p>
         
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-
+          
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={DashboardPipeline}
+              isBlog={false}
+              title="Data Pipeline + Dashboard"
+              description="A complete Data Engineering & BI project demonstrating the creation of an automated data pipeline capable of: collecting and consolidating data (from CSV / API), cleaning and normalizing inconsistent information, generating a clean and standardized dataset, persisting the data in a relational database (PostgreSQL), connecting the database to a Business Intelligence tool, in this case Power BI and creating an interactive monitoring dashboard"
+              ghLink="https://github.com/sthefanyspina/DataEngineeringProjects/tree/main/pipeline"
+            />
+          </Col>
+            
+            
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={SalesDasboard}
+              isBlog={false}
+              title="Sales Dasboard"
+              description="This project was developed to support both theoretical and practical studies on data analysis, data visualization, KPIs, and business performance metrics. The dashboard helps identify trends, patterns, and key commercial performance indicators. This project also focuses on analyzing historical sales data, identifying trends and seasonality, and creating forecasting models to predict future sales. It includes exploratory data analysis (EDA), data cleaning, and implementation of multiple time series forecasting models with performance comparison."
+              ghLink="https://github.com/sthefanyspina/DataAnalysisProjects/tree/main/Dashboards/sales_dashboard_powerbi"
+            />
+          </Col>
+  
             <Col md={4} className="project-card">
             <ProjectCard
               imgPath={CoffeSalesDashboard}
@@ -67,16 +89,6 @@ function Projects() {
           </Col>
 
             <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={SalesDasboard}
-              isBlog={false}
-              title="Sales Dasboard"
-              description="This project is an end-to-end data analysis solution designed to extract critical business insights from Sales data. Used Power Query for data processing and analysis and PowerBI to structured problem-solving techniques to solve key business questions and build the dashboard."
-              ghLink="https://github.com/sthefanyspina/DataAnalysisProjects/tree/main/Dashboards/sales_dashboard_powerbi"
-            />
-          </Col>
-
-            <Col md={4} className="project-card">
               <ProjectCard
                 imgPath={EletricVehiclesDasboard}
                 isBlog={false}
@@ -114,6 +126,7 @@ function Projects() {
 }
 
 export default Projects;
+
 
 
 
