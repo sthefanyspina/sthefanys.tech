e39b0960fa4fc1bed3f4eabf1b0acbdbced7d67c
Sthefany Spina <125087195+sthefanyspina@users.noreply.github.com>
2025-11-20 15:05:45 -0300

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -31,34 +31,21 @@ function ResumeNew() {
           </Button>
         </Row>
 
-        <Document file={pdf} className="d-flex justify-content-center">
+        <Row className="resume">
+          <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
 
-        <Document file={pdf} className="d-flex justify-content-center">
+        <Row className="resume2">
+          <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
-            
+
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
-      </Container>
-    </div>
-  );
 }
 
 export default ResumeNew;
-
-
-
-
